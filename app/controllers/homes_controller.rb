177679b99b7416a54a0afb4ca02b9735755e7b74
fgk244 <fgk.244@gmail.com>
2022-01-14 09:27:42 +0000

    14commit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
 
+  skip_before_action :login_required
+
   def top
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  #get 'sessions/new'
+  #get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => "homes#top"
   get 'homes/about' => 'homes#about'
+
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 
 
 
